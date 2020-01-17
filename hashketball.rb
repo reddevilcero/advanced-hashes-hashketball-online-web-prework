9383005c60c5028e9bf91871794c0a1de60f442c
Pedro David Garcia Lopez <reddevil_cero@hotmail.com>
2020-01-17 17:22:05 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -36,7 +36,7 @@ def num_points_scored(player_name)
 
       team_hash[:players].each { |player|
         if player[:player_name] == player_name
-          p player
+          returns player[:points]
         end
       }
    }
