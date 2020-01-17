87a197dd9ab31fb9d7b63be782b36547deddcb85
Pedro David Garcia Lopez <reddevil_cero@hotmail.com>
2020-01-17 17:07:23 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -10,7 +10,7 @@ def game_hash
       #   { :player_name => "Brook Lopez", :number => 11, :shoe => 17, :points => 17, :rebounds => 19, :assists => 10, :steals => 3, :blocks => 1, :slam_ducks => 15},
       #   { :player_name => "Mason Plumlee", :number => 1, :shoe => 19, :points => 26, :rebounds => 11, :assists => 6, :steals => 3, :blocks => 8, :slam_ducks => 5},
       #   { :player_name => "Jason Terry", :number => 31, :shoe => 15, :points => 19, :rebounds => 2, :assists => 2, :steals => 4, :blocks => 11, :slam_ducks => 1}
-      # 
+      #
       # ]
     },
     :away => {
@@ -23,8 +23,8 @@ def game_hash
         { :player_name => "Ben Gordon" :number => 8, :shoe => 15, :points => 33, :rebounds => 3, :assists => 2, :steals => 1, :blocks => 1, :slam_ducks => 0},
         { :player_name => "Kemba Walker", :number => 33, :shoe => 15, :points => 6, :rebounds => 12, :assists => 12, :steals => 7, :blocks => 5, :slam_ducks => 12}
       ]
-  
-    } 
+
+    }
   }
 
 end
